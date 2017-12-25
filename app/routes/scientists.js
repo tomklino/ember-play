29fff89b1d3ecca351dcc6b4b27c44e46db21824
Tom Klinovsky <tom.klino@gmail.com>
2017-12-25 14:09:06 +0200

    implemented model function for scientist and displayed its results in the template

--- app/routes/scientists.js
+++ app/routes/scientists.js
@@ -1,4 +1,11 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
+  model() {
+    //TODO load json file
+    results = [];
+    //TODO go over json and add relevant items to the result array
+    //return resutls
+    return ["Tom Klinovsky", "Stav Barak", "Beyleys", "Alba"]
+  }
 });
