1706cdf4ea5619e8a93dbb9825317b7cc8bd958b
Tom Klinovsky <tom.klino@gmail.com>
2017-12-25 14:42:49 +0200

    enclosed people list as a component and added routes for programmers and pets

--- app/router.js
+++ app/router.js
@@ -8,6 +8,8 @@ const Router = EmberRouter.extend({
 
 Router.map(function() {
   this.route('scientists');
+  this.route('programmers');
+  this.route('pets');
 });
 
 export default Router;
--- app/routes/pets.js
+++ app/routes/pets.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model() {
-    return ["Tom Klinovsky", "Stav Barak", "Beyleys", "Alba"]
+  model(){
+    return ['Alba', 'Balies', 'Sefi']
   }
 });
